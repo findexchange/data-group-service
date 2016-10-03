9947911901a513a25bbc34b36f1564463cf01a38
Freddie <sfm1617@hotmail.com>
2016-10-03 11:54:11 +0100

    bug fixed

--- test/flask_test.py
+++ test/flask_test.py
@@ -17,25 +17,26 @@ class FlaskrTestCase(unittest.TestCase):
 
 	#Test input data type and check response code
 	def test_data_type(self):
-		result = self.app.post('/app/v1.0/datasort', data= self.file)
-		print 'Testing data type in the foramt of json'
+		result = self.app.post('/api/v1.0/datasort', data= self.file)
+		print 'Testing data type in the foramt of json.'
 		self.assertEqual(result.status_code, 200)
 
 
 	#Testing whether counts are corret for each group 
 	#And return total number of groups
 	def test_num_in_each_group(self):
-		result = self.app.post('/app/v1.0/datasort', data= self.file)
-		print 'Comparing the number in groups with count column'
+		upload_data = self.app.post('/api/v1.0/datasort', data= self.file)
+		print 'Comparing the number in groups with count column.'
 		import json
-		print 'There are :' ,len(json.loads(result.data)), 'of group generated'
+		result = json.loads(upload_data.data)['results']
+		print 'There are :' ,len(result), 'of group generated.'
 		import pandas as pd
-		data_frame = pd.read_json(result.data)
-		count = data_frame['Count']
+		data_frame = pd.read_json(json.dumps(result))
+		count = data_frame['count']
 		rows = data_frame['rows']		
 		count_ids_rows = rows.apply(lambda x: str(x)).apply(lambda x : x.strip('[]').split(',')).apply(lambda x: len(x))
 		data_frame['no_in_group'] = count_ids_rows
-		compare_with_count =  data_frame[~data_frame['Count'] == data_frame['no_in_group']]
+		compare_with_count =  data_frame[~data_frame['count'] == data_frame['no_in_group']]
 		self.assertEqual(len(compare_with_count),0)
 
 
