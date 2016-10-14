94fe9463a6491e180f4841410ff564eda7aae5d3
Freddie <sfm1617@hotmail.com>
2016-10-14 15:39:49 +0100

    nothing

--- src/data_sort.py
+++ src/data_sort.py
@@ -211,7 +211,6 @@ class data_sort:
 	def get_tagged(self):
 		data = self.read_data()
 		data['tags'] =  data.name.apply(self.encode_data).apply(lambda x: x.lower()).apply(self.replace_no_char).apply(self.replace_no_char_on_tag).apply(self.tagging)
-		data.applymap(self.encode_data).to_csv('../../../results.csv') #this line is for local testing 
 		return data 
 
 
@@ -224,3 +223,4 @@ class data_sort:
 		combined_output.index.name = 'groupName_Id'
 		combined_output.reset_index(inplace = True)
 		return combined_output.to_json(orient = 'records')
+
