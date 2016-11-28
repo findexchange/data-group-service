cb6501547a34b7cd9f80c5054bb32bf67b2898a0
Freddie <sfm1617@hotmail.com>
2016-11-28 13:13:42 +0000

    path for compiling

--- src/clustering_m/setup.py
+++ src/clustering_m/setup.py
@@ -4,6 +4,6 @@ import numpy
 
 setup(
   name = 'clustering app',
-  ext_modules = cythonize("clustering.pyx"),
+  ext_modules = cythonize("./clustering.pyx"),
   include_dirs=[numpy.get_include()]
 )
